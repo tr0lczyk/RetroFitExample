906b9b0388a022f43a79cf767dd3ccd395c9a2c1
Mateusz <30321069+tr0lczyk@users.noreply.github.com>
2018-10-24 13:30:08 +0200

    added queries, parameters and other methods

---
+++ app/src/main/java/com/olczyk/android/retrofitsample/Comment.java
@@ -0,0 +1,65 @@
+package com.olczyk.android.retrofitsample;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Comment {
+
+    private int postId;
+
+    private int id;
+
+    private String name;
+
+    private String email;
+
+    @SerializedName("body")
+    private String text;
+
+    public Comment(int postId, int id, String name, String email, String text) {
+        this.postId = postId;
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.text = text;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- app/src/main/java/com/olczyk/android/retrofitsample/MainActivity.java
+++ app/src/main/java/com/olczyk/android/retrofitsample/MainActivity.java
@@ -8,7 +8,9 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,15 +25,63 @@ public class MainActivity extends AppCompatActivity {
     @ViewById
     TextView textViewResult;
 
+    private PostActionRetrofit postActionRetrofit;
+
     @AfterViews
     public void aVoid(){
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        PostActionRetrofit postActionRetrofit = retrofit.create(PostActionRetrofit.class);
+        postActionRetrofit = retrofit.create(PostActionRetrofit.class);
 
-        Call<List<Post>> call = postActionRetrofit.getPosts();
+//        getPost();
+        getComments();
+    }
+
+    private void getComments() {
+        Call<List<Comment>> call = postActionRetrofit.getComments("posts/2/comments");
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code " + response.code());
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+
+                for(Comment comment :comments){
+                    String content = "\n";
+                    content += "ID: "+ comment.getId() + "\n";
+                    content += "PostId: " + comment.getPostId() + "\n";
+                    content += "Name: " + comment.getName() + "\n";
+                    content += "Email: " + comment.getEmail() + "\n";
+                    content += "Content: " + comment.getText() + "\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getPost() {
+
+        Map<String,String> parameters = new HashMap<>();
+        parameters.put("userId","1");
+        parameters.put("_sort", "id");
+        parameters.put("_order", "desc");
+
+
+//        Call<List<Post>> call = postActionRetrofit.getPosts(new Integer[]{1,2,6},null,
+//                null);
+
+        Call<List<Post>> call = postActionRetrofit.getPosts(parameters);
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
